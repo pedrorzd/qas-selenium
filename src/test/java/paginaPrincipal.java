9f3efb6b1ca2f3eb2fd4af42a237ca117c70cb1a
Pedro Rodrigues <pedrohrodrigues86@gmail.com>
2025-10-06 18:07:19 -0300

    primeiro teste

--- src/test/java/paginaPrincipal.java
+++ src/test/java/paginaPrincipal.java
@@ -1,10 +1,16 @@
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 
 import java.time.Duration;
@@ -17,6 +23,7 @@ class paginaPrincipal {
     void start() {
         WebDriver driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        driver.get("https://www.araujo.com.br/");
     }
     @AfterEach
     void finish() {
@@ -24,9 +31,17 @@ class paginaPrincipal {
     }
 
     @Test
-    @DisplayName("Teste Logo")
-    void testLogo() {
-        driver.get("http://localhost/litecart/");
+    @DisplayName("Colocando produto no carrinho")
+    void testProdutoCarrinho() {
+
+        WebElement produto = driver.findElement(By.xpath("//div[@id='carousel-9b26250b30ce087420103f636d']//img[@alt='C E Ferulic SkinCeuticals Serum Antioxidante 30ml']"));
+        produto.click();
+
+        Wait<WebDriver> wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        wait.until(ExpectedConditions.urlToBe("https://www.araujo.com.br/c-e-ferulic-skinceuticals-serum-antioxidante-30ml/65565.html"));
+
+        WebElement adicionandoCesta = driver.findElement(By.xpath("//button[@class='primaryButtonAddCart add-to-cart primaryButton']"));
+        adicionandoCesta.click();
     }
 
 }
